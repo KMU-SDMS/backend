47c221f14a6f57b98023ee137f3dabb1e0e02cda
Hayeon101 <hyungu142@gmail.com>
2025-09-10 14:41:25 +0900

    feat: create_success_response 함수 수정 및 상태 코드 매개변수 추가
    - responses.py: create_success_response 함수에 상태 코드 매개변수 추가 및 데이터 유형에 따른 로깅 메시지 개선

--- src/utils/responses.py
+++ src/utils/responses.py
@@ -6,11 +6,16 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def create_success_response(data):
-    """성공적인 API 응답을 생성합니다 (상태 코드 200)."""
-    logger.info(f"✅ Successfully prepared response with {len(data)} items.")
+def create_success_response(data, status_code=200):
+    """성공적인 API 응답을 생성합니다."""
+    # data가 리스트인지 단일 객체인지 확인
+    if isinstance(data, list):
+        logger.info(f"✅ Successfully prepared response with {len(data)} items.")
+    else:
+        logger.info("✅ Successfully prepared response with single item.")
+
     return {
-        "statusCode": 200,
+        "statusCode": status_code,
         "headers": {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
